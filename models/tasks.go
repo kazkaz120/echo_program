8a8e153e5a2f7ea856b1b8a76257dbfc47d26d7c
Kazuki <syukatukazu2@yahoo.co.jp>
2022-06-16 13:21:46 +0900

    0616

--- models/tasks.go
+++ models/tasks.go
@@ -5,6 +5,7 @@ type Task struct {
 	ID    int    `json:"id"`
 	Name  string `json:"name"`
 	Name2 string `json:"name2"`
+	Name3 string `json:"name3"`
 }
 
 // TaskCollection is collection of Tasks
@@ -16,12 +17,12 @@ type TaskCollection struct {
 func GetTasks() (tc TaskCollection) {
 	tc = TaskCollection{
 		[]Task{
-			{1, "洗濯", "消費"},
-			{2, "勉強", "投資"},
-			{3, "ゲーム", "浪費"},
-			{4, "勉強", "投資"},
-			{5, "読書", "投資"},
-			{6, "食事", "消費"},
+			{1, "洗濯", "消費", "1時間"},
+			{2, "勉強", "投資", "2時間"},
+			{3, "ゲーム", "浪費", "2時間"},
+			{4, "勉強", "投資", "3時間"},
+			{5, "読書", "投資", "1時間"},
+			{6, "食事", "消費", "1時間"},
 		},
 	}
 
